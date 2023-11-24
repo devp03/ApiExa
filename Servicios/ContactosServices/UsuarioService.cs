8b7211ed5c311c5c9c36d82daabb00520d910954
Lucas <Lucas24@Dev.com>
2023-11-23 21:44:59 -0300

    Exa

---
+++ Servicios/ContactosServices/UsuarioService.cs
@@ -0,0 +1,42 @@
+﻿using Infraestructura.Datos;
+using Infraestructura.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.ContactosServices
+{
+    public class UsuarioService
+    {
+        UsuarioDatos usuarioDatos;
+        public UsuarioService(string cadenaConexion)
+        {
+            usuarioDatos = new UsuarioDatos(cadenaConexion);
+        }
+
+        public void insertarUsuario(UsuarioModel usuario)
+        {
+           
+            usuarioDatos.insertarUsuario(usuario);
+        }
+
+        public UsuarioModel obtenerUsuarioPorId(int id)
+        {
+            return usuarioDatos.obtenerUsuarioPorId(id);
+        }
+
+        public void modificarUsuario(UsuarioModel usuario)
+        {
+            
+            usuarioDatos.modificarUsuario(usuario);
+        }
+
+        public void eliminarUsuario(UsuarioModel usuario)
+        {
+            
+            usuarioDatos.eliminarUsuario(usuario);
+        }
+    }
+}
